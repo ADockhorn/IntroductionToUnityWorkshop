a08bf87390bda73689ca1fb595676c3b68247057
Unknown <alexander.dockhorn@ovgu.de>
2018-04-03 18:41:19 +0200

    added second Input Axis, playerRotation and nose sprite

---
+++ WorkshopProject/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal2");
+        float y = Input.GetAxis("Vertical2");
+
+        if (x != 0 || y != 0)
+        {
+            this.transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(y, x) * 180 / Mathf.PI);
+        }
+	}
+}
