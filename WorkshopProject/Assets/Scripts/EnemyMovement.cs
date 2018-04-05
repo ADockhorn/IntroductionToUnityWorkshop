1230b19876c2ffe278b1821c3c40dc0d6bb03156
Matthis Hagen <matthis.hagen@o2online.de>
2018-04-05 08:49:41 +0200

    Added enemies all at once!

---
+++ WorkshopProject/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 1;
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveVector = target.transform.position - this.transform.position;
+        this.transform.position = this.transform.position + moveVector.normalized * speed * Time.deltaTime;
+
+        if(Vector3.Distance(this.transform.position, target.transform.position) > 0.1)
+            this.transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(moveVector.y, moveVector.x) * 180 / Mathf.PI);
+    }
+}
---
+++ WorkshopProject/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float initialHealth;
+
+    float currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = initialHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ApplyDamage(float damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth < 0)
+        {
+            Debug.Log("Player Died");
+
+            // this allows us to test stuff easier!
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
